00e83a6f40c5b31c6ccc537bd5179047fdd842f9
André Zopolato <andrezopo@gmail.com>
2022-08-09 19:22:03 -0300

    adiciona export para a função de validar schemas

--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -1,11 +1,12 @@
-
 function validateSchema(schema) {
   return (req, res, next) => {
     const body = req.body;
     const { error } = schema.validate(body, { abortEarly: false });
-    if(error){
-      res.status(422).send(error.details);
+    if (error) {
+      res.status(422).send(error.details.map((response) => response.message));
     }
     next();
-  }
+  };
 }
+
+export default validateSchema;
