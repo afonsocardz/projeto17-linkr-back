355139fe5a516ef568af23190864a85c88d78d76
André Zopolato <andrezopo@gmail.com>
2022-08-09 19:18:05 -0300

    acrescenta resposta com todas as mensagens de erro da validação de schema

--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -1,11 +1,10 @@
-
 function validateSchema(schema) {
   return (req, res, next) => {
     const body = req.body;
     const { error } = schema.validate(body, { abortEarly: false });
-    if(error){
-      res.status(422).send(error.details);
+    if (error) {
+      res.status(422).send(error.details.map((response) => response.message));
     }
     next();
-  }
+  };
 }
