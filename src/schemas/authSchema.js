af69d3b9b7b3b5bfb188da5ee0c64dcd88759eca
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-08-09 20:14:03 -0300

    Esquema de validação dos imputs recebidos

---
+++ src/schemas/authSchema.js
@@ -0,0 +1,13 @@
+import joi from "joi";
+
+const signUpSchema = joi.object({
+  email: joi.string().email().max(50).required(),
+  password: joi
+  .string()
+  .regex(/^(?=.\d)(?=.[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)
+  .required(),
+  username: joi.string().max(20).required(),
+  pictureUrl: joi.string().required()
+});
+
+export { signUpSchema };
