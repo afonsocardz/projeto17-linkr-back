c814ff22d384567425315ec23b78dd90458b4bd8
André Zopolato <andrezopo@gmail.com>
2022-08-09 21:37:20 -0300

    finaliza rota de signin, cria validação do token e atualiza tempo do token na validação

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import authRouter from "./routers/authRouter.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use([cors(), express.json()]);
+
+app.use(authRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`servidor funfando de boas na porta ${process.env.PORT}`);
---
+++ src/repositories/tokenRepository.js
@@ -0,0 +1,19 @@
+import connection from "../config/postgres.js";
+
+export function getUserByToken(token) {
+  return connection.query(
+    `
+      SELECT u.* FROM sessions s JOIN users u ON s."userId" = u.id WHERE token = $1
+      `,
+    [token]
+  );
+}
+
+export function updateToken(token) {
+  return connection.query(
+    `
+      UPDATE sessions SET "createdAt" = $1 WHERE token = $2
+      `,
+    [new Date(), token]
+  );
+}
