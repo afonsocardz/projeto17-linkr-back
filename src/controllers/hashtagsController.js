a5ecd4e0bf84c0a3597497448cb3358cdfd5f810
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-08-12 11:23:40 -0300

    Cria controlador para popular novas hashtags

--- src/controllers/hashtagsController.js
+++ src/controllers/hashtagsController.js
@@ -1,4 +1,4 @@
-import { trendingHashtags } from "../repositories/hastagsRepositories.js";
+import { createHashtags, searchForHashtag, selectHashtags, trendingHashtags } from "../repositories/hastagsRepositories.js";
 
 async function getTrendingHashtags(req, res) {
   try {
@@ -11,4 +11,25 @@ async function getTrendingHashtags(req, res) {
   }
 }
 
-export { getTrendingHashtags };
+async function populatingHashtags(req, res) {
+  try {
+    const { rows: hashtag, rowCount: foundHashtag } = await selectHashtags();
+    console.log(hashtag);
+  
+    if (foundHashtag === 0) return res.status(404).send("No hashtag was found");
+  
+    const { rows, rowCount: metHashtag } = await searchForHashtag(hashtag);
+    console.log(rows);
+    
+    if (metHashtag === 1) return res.status(409).send("Hashtag already exists");
+  
+    await createHashtags(hashtag);
+  
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500)
+  }
+}
+
+export { getTrendingHashtags, populatingHashtags };
--- src/repositories/hastagsRepositories.js
+++ src/repositories/hastagsRepositories.js
@@ -1,7 +1,7 @@
 import connection from "../config/postgres.js";
 
 async function trendingHashtags() {
-    return connection.query(
+  return connection.query(
     `
         SELECT COUNT(h."hashtagName") AS trending 
         FROM hashtags h
@@ -11,5 +11,17 @@ async function trendingHashtags() {
   );
 }
 
+async function selectHashtags (){
+  return connection.query(`SELECT message FROM posts WHERE message LIKE '#%'`);
+}
 
-export { trendingHashtags };
+async function searchForHashtag(hashtag) {
+  return connection.query(`SELECT * FROM hashtags WHERE "hashtagName" = $1`, [hashtag]);
+}
+
+async function createHashtags(hashtag) {
+  return connection.query(`INSERT INTO hashtags ("hashtagName") VALUES $1`, [hashtag]);
+}
+
+
+export { createHashtags, searchForHashtag, selectHashtags, trendingHashtags };
