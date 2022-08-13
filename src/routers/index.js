f73dcd66994c16cb693a86d7a85d8a051bd9dce8
Carlos André Lima de Matos <carlosandretaiga@gmail.com>
2022-08-13 16:54:02 -0300

    feat: ir para página do usuário ao clicar no nome dele

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import postsRouters from "./postsRouters.js";
 import authRouters from "./authRouters.js";
 import verifyExpiredTokens from "../middlewares/verifyExpiredTokens.js";
+import userRouter from "./userRouter.js";
 
 const router = Router();
 
 router.use("/posts", postsRouters);
 router.use("/", authRouters);
 //router.use("/session", authRouters);
+router.use(userRouter);
 
 setInterval(verifyExpiredTokens, 60000);
 
