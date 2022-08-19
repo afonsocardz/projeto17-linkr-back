905e3455da0a6029d473aeb5dd7dc2e8069aa56b
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-08-19 19:29:46 -0300

    Limitação das páginas de user e hashtags

--- src/controllers/hashtagsController.js
+++ src/controllers/hashtagsController.js
@@ -18,6 +18,11 @@ async function getTrendingHashtags(req, res) {
 async function redirectHashtag(req, res) {
   const { hashtag } = req.params;
   const newHashtag = "#"+hashtag
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
 
   try {
     const { rowCount: metHashtag } = await searchForHashtag(newHashtag);
@@ -28,7 +33,9 @@ async function redirectHashtag(req, res) {
 
     if (postsHashtags.length === 0) return res.status(404).send("No posts related to this hashtag");
 
-    res.status(200).send(postsHashtags);
+    const newPostsHashtags = await postsHashtags.slice(startIndex, endIndex)
+
+    res.status(200).send(newPostsHashtags);
 
   } catch (error) {
     console.error(error);
